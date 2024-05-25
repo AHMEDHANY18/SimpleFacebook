2b1db89a8845456f03a326aed02ce6302ecaf2cf
Ahmed Hany <90726408+AHMEDHANY18@users.noreply.github.com>
2024-05-25 18:42:31 +0300

    Update connectionDB.js

--- db/connectionDB.js
+++ db/connectionDB.js
@@ -1,10 +1,7 @@
 import mysql from "mysql2"
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('simpleFacebook', 'root', '', {
-    host: 'localhost',
-    dialect: "mysql"
-});
+export const sequelize = new Sequelize('mysql://uvxl34jpk3y1rzuw:AFxwCTKSSmxuPpcY1qAo@bqbz73azmxvtm1t2okaz-mysql.services.clever-cloud.com:3306/bqbz73azmxvtm1t2okaz');
 
 const connectionDB=async ()=>{
     return await sequelize.sync().then(()=>{
